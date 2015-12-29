6fbfa1a3d5b376b82127622cf6265fdabee9881b
tomonari_t <tomo1208.japan@gmail.com>
2015-12-29 10:51:27 +0100

    Add gulpfile.babel.js

---
+++ gulpfile.babel.js
@@ -0,0 +1,8 @@
+/**
+* To use gulp.babel.js for gulp's file name makes us write gulpfile.js with ES6
+*/
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins'
+
+const $ = gulpLoadPlugins();
