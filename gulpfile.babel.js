83adf8fc65a4d911dd5ab0d2e836f4cf333b1459
tomonari_t <tomo1208.japan@gmail.com>
2015-12-29 16:27:01 +0100

    Add for webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,19 @@
 */
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import webpackConfig from './webpack.config.js';
+
 
 const $ = gulpLoadPlugins();
 
 gulp.task('default', () => {
-  gulp.src('src/js/**/*.js')
-    .pipe($.babel())
-    .pipe(gulp.dest('dist/js'));
+  gulp.src('./src/js/**/*.js')
+  .pipe($.webpack(webpackConfig))
+  .pipe(gulp.dest('./dist/js/'));
 });
+
+// gulp.task('default', () => {
+//   gulp.src('src/js/**/*.js')
+//     .pipe($.babel())
+//     .pipe(gulp.dest('dist/js'));
+// });
