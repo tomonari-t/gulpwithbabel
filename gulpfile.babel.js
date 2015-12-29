0b8df48f91e30448e127b13f92e1146afccee0fd
tomonari_t <tomo1208.japan@gmail.com>
2015-12-29 12:34:40 +0100

    Update gulpgile.babel.ja

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,13 @@
 /**
 * To use gulp.babel.js for gulp's file name makes us write gulpfile.js with ES6
 */
-
 import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins'
+import gulpLoadPlugins from 'gulp-load-plugins';
 
 const $ = gulpLoadPlugins();
 
-gulp.task('script', () => {
+gulp.task('default', () => {
   gulp.src('src/js/**/*.js')
     .pipe($.babel())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist/js'));
 });
