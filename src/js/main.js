d49c2087a87115bc603982163f564b229cab7828
tomonari_t <tomo1208.japan@gmail.com>
2015-12-29 15:23:38 +0100

    Write something

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 const a = 10;
+
+class Test{
+  constructor ( name = 'noname'){
+    this.name = name;
+  }
+  say() {
+    console.log(this.name)
+  }
+}
+
+const test = new Test('tomonari');
+test.say();
